0fd88cf8bcc24ef080b0162a08530491ef7afa79
sambuite <murilosambuite@gmail.com>
2020-08-06 15:52:25 -0300

    feat: add input and select components

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import { Container, Form, PageMain } from './styles';
 
@@ -10,20 +12,36 @@ const TeacherList = () => {
     <Container>
       <PageHeader title="Estes são os proffys disponíveis.">
         <Form>
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"></input>
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Ciência', label: 'Ciência' },
+              { value: 'Educação física', label: 'Educação física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda' },
+              { value: '2', label: 'Terça' },
+              { value: '3', label: 'Quarta' },
+              { value: '4', label: 'Quinta' },
+              { value: '5', label: 'Sexta' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+          <Input name="time" label="Hora" type="time" />
         </Form>
       </PageHeader>
 
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -16,51 +16,12 @@ export const Form = styled.form`
     color: ${({ theme: t }) => t.colors.textInPrimary};
   }
 
-  .input-block {
-    position: relative;
-
-    & + .input-block {
-      margin-top: 1.4rem;
-    }
-
-    label {
-      font-size: 1.4rem;
-    }
-
-    input {
-      width: 100%;
-      height: 5.6rem;
-      margin-top: 0.8rem;
-      border-radius: 0.8rem;
-      background: ${({ theme: t }) => t.colors.inputBackground};
-      border: 1px solid ${({ theme: t }) => t.colors.lineInWhite};
-      outline: 0;
-      padding: 0 1.6rem;
-      font: 1.6rem Archivo;
-    }
-
-    &:focus-within::after {
-      content: '';
-      width: calc(100% - 3.2rem);
-      height: 2px;
-      background: ${({ theme: t }) => t.colors.primaryLight};
-      position: absolute;
-      left: 1.6rem;
-      right: 1.6rem;
-      bottom: 0;
-    }
-  }
-
   @media (min-width: 700px) {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     column-gap: 16px;
     position: absolute;
     bottom: -28px;
-
-    & .input-block + .input-block {
-      margin-top: 0;
-    }
   }
 `;
 
