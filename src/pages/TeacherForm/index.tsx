8f8cb506e7d6376e34b817a7382d04c068dc5c8e
sambuite <murilosambuite@gmail.com>
2020-08-06 15:54:55 -0300

    feat: create main style

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,13 +1,86 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
-import { Container } from './styles';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import { Container, Main, Footer } from './styles';
 
 const TeacherForm = () => {
   return (
     <Container>
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <Main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo" type="text" />
+          <Input name="avatar" label="Avatar" type="text" />
+          <Input name="whatsapp" label="Whatsapp" type="text" />
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Ciência', label: 'Ciência' },
+              { value: 'Educação física', label: 'Educação física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula" type="text" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda' },
+                { value: '2', label: 'Terça' },
+                { value: '3', label: 'Quarta' },
+                { value: '4', label: 'Quinta' },
+                { value: '5', label: 'Sexta' },
+                { value: '6', label: 'Sábado' },
+              ]}
+            />
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
+
+        <Footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </Footer>
+      </Main>
     </Container>
   );
 };
--- src/pages/TeacherForm/styles.ts
+++ src/pages/TeacherForm/styles.ts
@@ -3,4 +3,138 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+
+  > div .header-content {
+    margin-bottom: 6.4rem;
+  }
+
+  @media (min-width: 700px) {
+    max-width: 100vw;
+
+    > div .header-content {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export const Main = styled.main`
+  background: ${({ theme: t }) => t.colors.boxBase};
+  width: 100%;
+  max-width: 74rem;
+  border-radius: 0.8rem;
+  margin: -3.2rem auto 3.2rem;
+  padding-top: 6.4rem;
+  overflow: hidden;
+
+  fieldset {
+    border: 0;
+    padding: 0 2.4rem;
+
+    & + fieldset {
+      margin-top: 6.4rem;
+    }
+
+    legend {
+      font: 700 2.4rem Archivo;
+      color: ${({ theme: t }) => t.colors.textTitle};
+      margin-bottom: 2.4rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      padding-bottom: 1.6rem;
+      border-bottom: 1px solid ${({ theme: t }) => t.colors.lineInWhite};
+
+      button {
+        background: none;
+        border: 0;
+        color: ${({ theme: t }) => t.colors.primary};
+        font: 700 1.5rem Archivo;
+        cursor: pointer;
+        transition: color 0.2;
+
+        &:hover {
+          color: ${({ theme: t }) => t.colors.primaryDark};
+        }
+      }
+    }
+
+    .input-block + .textarea-block,
+    .select-block + .input-block {
+      margin-top: 2.4rem;
+    }
+  }
+
+  label {
+    color: ${({ theme: t }) => t.colors.textComplement};
+  }
+
+  @media (min-width: 700px) {
+    fieldset {
+      padding: 0 6.4rem;
+    }
+
+    .schedule-item {
+      display: grid;
+      grid-template-columns: 2fr 1fr 1fr;
+      column-gap: 1.6rem;
+    }
+  }
+`;
+
+export const Footer = styled.footer`
+  padding: 4rem 2.4rem;
+  background: ${({ theme: t }) => t.colors.boxFooter};
+  border-top: 1px solid ${({ theme: t }) => t.colors.lineInWhite};
+  margin-top: 6.4rem;
+
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    color: ${({ theme: t }) => t.colors.textComplement};
+
+    img {
+      margin-right: 2rem;
+    }
+  }
+
+  button {
+    width: 100%;
+    height: 5.6rem;
+    background-color: ${({ theme: t }) => t.colors.secondary};
+    color: ${({ theme: t }) => t.colors.buttonText};
+    border: 0;
+    border-radius: 0.8rem;
+    cursor: pointer;
+    font: 700 1.6rem Archivo;
+    margin-top: 3.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${({ theme: t }) => t.colors.secondaryDark};
+    }
+  }
+
+  @media (min-width: 700px) {
+    padding: 4rem 6.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+      justify-content: space-between;
+    }
+
+    button {
+      width: 20rem;
+      margin-top: 0;
+    }
+  }
 `;
